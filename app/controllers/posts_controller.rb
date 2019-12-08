70614d898d44633a152a0150ad35f123f1c3d469
EC2 Default User <ec2-user@ip-172-31-88-128.ec2.internal>
2019-12-08 15:22:51 +0000

    toukouform

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+  def new
+    if logged_in?
+      @micropost = current_user.microposts.build  # form_with 用
+    end
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
+    else
+      @posts = current_user.posts.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'toppages/index'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
