f7eae9f8f7757dcc9b24c0cbe9476ba08dd7941d
EC2 Default User <ec2-user@ip-172-31-88-128.ec2.internal>
2020-01-06 23:49:01 +0000

    rails c ok

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :favorites
+  has_many :users, through: :favorites
    validates :country, presence: true, length: { maximum: 255 }
    validates :area, presence: true, length: { maximum: 255 }
    validates :comment, presence: true, length: { maximum: 255 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   has_many :posts
+  has_many :favorites
+  has_many :fav_posts, through: :favorites, source: :post
+  def favorite(post)
+  self.favorites.find_or_create_by(post_id: post.id)
+  end
+  def unfavorite(post)
+   favorite = favorites.find_by(post_id: post.id)
+   favorite.destroy if favorite
+  end
+  def favorite?(post)
+    self.fav_posts.include?(post)
+  end
 end
