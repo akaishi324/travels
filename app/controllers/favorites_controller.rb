22bf393eb395d52574a44a5f87dea8cac1125958
EC2 Default User <ec2-user@ip-172-31-88-128.ec2.internal>
2020-01-16 12:55:38 +0000

    syuusei2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,14 @@ class FavoritesController < ApplicationController
   def create
   post = Post.find(params[:post_id])
   current_user.favorite(post)
-  flash[:success] = "登録完了しました！"
+  flash[:success] = "行きたいリストに追加しました！"
   redirect_to root_path
   end
 
   def destroy
   post = Post.find(params[:post_id])
   current_user.unfavorite(post)
-  flash[:success] = "登録解除しました！"
+  flash[:success] = "行きたいリストから外しました！"
   redirect_to root_path
   end
 end
